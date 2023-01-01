f3b60309d29327324ea33b513f8a7e36c33db768
Harry Manchanda <harmanmanchanda182@gmail.com>
2023-01-01 15:11:40 +0530

    Update Query

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,19 +1,11 @@
 export const getUserCollection = `
   query GetUserCollection($first: Int!) {
     userCollection(first: $first) {
-      pageInfo {
-        hasPreviousPage
-        hasNextPage
-      }
       edges {
         node {
           email
           name
-          id
-          updatedAt
-          createdAt
         }
-        cursor
       }
     }
   }
